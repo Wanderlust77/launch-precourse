ab1ca020e27e7e624d5c8507599063510c16728c
Eva Keller <eva.keller1@gmail.com>
2019-05-12 09:40:11 +0000

    exercise 7 done

--- arrays.rb
+++ arrays.rb
@@ -39,7 +39,7 @@ arr = [15, 7, 18, 5, 12, 8, 5, 1]
 
 arr.index(5) #=>3
 
-arr.index[5]
+#arr.index[5]
 # => NoMethodError (undefined method `[]' for #<Enumerator: [15, 7, 18, 5, 12, 8, 5, 1]:index>)
 arr[5] #=>8
 
@@ -50,4 +50,20 @@ a = string[6] #=>"e"
 b = string[11] #=> "A"
 c = string[19] #=> nil
 
+=begin
+Write a program that iterates over an array and builds a new array that is the 
+result of incrementing each value in the original array by a value of 2. You 
+should have two arrays at the end of this program, The original array and the 
+new array you've created. Print both arrays to the screen using the p method instead of puts.
+=end
 
+arr = [1, 2, 3, 4, 5, 6, 7]
+
+new_arr = []
+
+arr.each do |n|
+  new_arr << n + 2
+end
+
+p arr
+p new_arr
